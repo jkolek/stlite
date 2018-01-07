03338e5ffcd4e81f96cd5c5416a8ce4f6ada76be
Jozef Kolek <jkolek@gmail.com>
2018-01-07 13:01:11 +0100

    Copyright year updated

--- array.h
+++ array.h
@@ -1,7 +1,7 @@
 // The MIT License (MIT)
 //
 // STLite array
-// Copyright (c) 2017 Jozef Kolek <jkolek@gmail.com>
+// Copyright (c) 2017, 2018 Jozef Kolek <jkolek@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
